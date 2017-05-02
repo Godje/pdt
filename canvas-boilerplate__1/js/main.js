4914d7e34a06d9698949d3402240c21995af8d05
Godje <comepot.4anell@gmail.com>
2017-05-02 15:58:48 -0700

    canvas-boilerpate__1
    Создаю репозиторий для канала. Добивл бойлерплейт для канваса который мы
    использовали в некоторых уроках.

---
+++ canvas-boilerplate__1/js/main.js
@@ -0,0 +1,40 @@
+(function(){
+	var canvasBody = document.getElementById("canvas"),
+			canvas = canvasBody.getContext("2d"),
+
+			w = canvasBody.width = window.innerWidth,
+			h = canvasBody.height = window.innerHeight,
+			gui = new dat.GUI(),
+			pi = Math.PI,
+			pi2 = pi*2,
+			piD2 = pi/2,
+			tick = 0,
+			opts = {
+				bgc: "#222",
+				showFPS: true
+			};
+
+	function setup(){
+
+		stats = new Stats();
+		stats.showPanel( 0 );
+		gui.add(opts, 'showFPS')
+		document.body.appendChild( stats.domElement );
+		window.requestAnimationFrame(loop);
+	};
+	function loop(){
+		stats.begin();
+		canvas.fillStyle = opts.bgc;
+		canvas.fillRect(0,0,w,h);
+
+		!opts.showFPS?stats.domElement.style.display = "none" : stats.domElement.style.display = "block";
+		window.requestAnimationFrame(loop);
+		stats.end();
+	};
+	setup();
+
+	window.addEventListener("resize", function(){
+		w = canvasBody.width = window.innerWidth;
+		h = canvasBody.height = window.innerHeight;
+	});
+})();
