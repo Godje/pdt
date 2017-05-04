45619abbea5f7b16b4fdeb4eb0505b681f21c5b5
Godje <comepot.4anell@gmail.com>
2017-05-04 13:28:21 -0700

    Update main.js [tut-list]

--- pdt-sources/main.js
+++ pdt-sources/main.js
@@ -1,16 +1,19 @@
 var items = [
-  ["[Canvas] Как сделать классную анимацию для фона в Canvas", "https://github.com/Godje/pdt/tree/master/pdt-sources/canvas-animation__1"],
-  ["[Canvas] Как сделать рисование в Canvas","https://github.com/Godje/pdt/tree/master/pdt-sources/canvas-drawing"],
-  ["[Canvas] Как сделать Pixel Art в Canvas","https://github.com/Godje/pdt/tree/master/pdt-sources/canvas-pixel-art"],
-  ["[Canvas] Как создать сложенную анимацию в Canvas","https://github.com/Godje/pdt/tree/master/pdt-sources/canvas-animation__2"],
-],
-  parent = document.getElementById("search--field"),
-  input = document.getElementById("search--input");
+		["[Canvas] Как сделать классную анимацию для фона в Canvas", "https://github.com/Godje/pdt/tree/master/pdt-sources/canvas-animation__1"],
+		["[Canvas] Как сделать рисование в Canvas", "https://github.com/Godje/pdt/tree/master/pdt-sources/canvas-drawing"],
+		["[Canvas] Как сделать Pixel Art в Canvas", "https://github.com/Godje/pdt/tree/master/pdt-sources/canvas-pixel-art"],
+		["[Canvas] Как создать сложенную анимацию в Canvas", "https://github.com/Godje/pdt/tree/master/pdt-sources/canvas-animation__2"],
+		["[Canvas] Как создать преследующие частицы в HTML5 Canvas", "https://github.com/Godje/pdt/tree/master/pdt-sources/vector--pursuers"],
+		['[Canvas] Как создать "Цветной Хаос" в канвасе', "https://github.com/Godje/pdt/tree/master/pdt-sources/vector--color-chaos"],
+	],
+	parent = document.getElementById("search--field"),
+	input = document.getElementById("search--input");
+
 renderItems(items, parent)
-input.addEventListener("input", function(e){
+input.addEventListener("input", function(e) {
 	const val = e.target.value.toLowerCase();
-	if(val !== ""){
-		let newArr = items.filter(function(e){
+	if (val !== "") {
+		let newArr = items.filter(function(e) {
 			return e[0].toLowerCase().match(val);
 		});
 		renderItems(newArr, parent);
@@ -18,14 +21,15 @@ input.addEventListener("input", function(e){
 		renderItems(items, parent)
 	}
 });
-function renderItems(arr, parent){
+
+function renderItems(arr, parent) {
 	parent.innerHTML = "";
-	for(var i = 0; i < arr.length; i++){
+	for (var i = 0; i < arr.length; i++) {
 		let el = document.createElement("a");
-    el.className = "search--item";
-    el.href = arr[i][1];
-    el.target = "_blank"
-    el.innerHTML = "<p>"+arr[i][0]+"</p>"
+		el.className = "search--item";
+		el.href = arr[i][1];
+		el.target = "_blank"
+		el.innerHTML = "<p>" + arr[i][0] + "</p>"
 		parent.appendChild(el);
 	}
 }
